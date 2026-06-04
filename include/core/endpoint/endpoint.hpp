d510c2690f59c1089d364eac6246b7719785368d
VeylorisRuinarisNyctalis <veylorisruinarisnyctalis@gmail.com>
2026-06-04 17:49:07 +0530

    tweaked errors

--- include/core/endpoint/endpoint.hpp
+++ include/core/endpoint/endpoint.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
 
+#include <cstdint>
 #include <iostream>
 #include <string>
 
@@ -14,15 +13,15 @@ protected:
 
 public:
     Endpoint() : IP_ADDRESS("127.0.0.1"), PORT(8080) {}
-    Endpoint(const string& IP_ADDRESS, const uint16_t& PORT) : IP_ADDRESS(IP_ADDRESS), PORT(PORT) {}
+    Endpoint(const std::string& IP_ADDRESS, const uint16_t& PORT) : IP_ADDRESS(IP_ADDRESS), PORT(PORT) {}
 
     virtual ~Endpoint() = default;
 
     // ------------ Getters ---------------
-    const std::string& get_ip_address() const {
+    const std::string& getIPAddress() const {
         return this->IP_ADDRESS;
     }
-    const uint16_t get_port() const {
+    uint16_t getPort() const {
         return this->PORT;
     }
 
