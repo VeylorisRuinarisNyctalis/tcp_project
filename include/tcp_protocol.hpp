1edab8e54d059b5f9a3eeb50bbfbf4941f718bb9
VeylorisRuinarisNyctalis <veylorisruinarisnyctalis@gmail.com>
2026-06-04 14:39:07 +0530

    auto-formatting

--- include/tcp_protocol.hpp
+++ include/tcp_protocol.hpp
@@ -1,87 +1,82 @@
 #pragma once
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#include<iostream>
-#include<string>
-
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-
 class TCP_Protocol {
 protected:
-	 string IP_ADDRESS;	// Stores IP ADDRESS
-	 int PORT, SOCKET_FD;	// Stores PORT NO, and SOCKET_FD
+    string IP_ADDRESS;    // Stores IP ADDRESS
+    int PORT, SOCKET_FD;  // Stores PORT NO, and SOCKET_FD
 
 public:
-	// Constructor
-	TCP_Protocol(): IP_ADDRESS("10.23.0.174"), PORT(8080), SOCKET_FD(-1) {}
-	TCP_Protocol(const string& IP, const int PORT): IP_ADDRESS(IP), PORT(PORT), SOCKET_FD(-1) {}
-	
-	// Destructor
-	virtual ~TCP_Protocol() {
-		closeSocket();
-	}
+    // Constructor
+    TCP_Protocol() : IP_ADDRESS("10.23.0.174"), PORT(8080), SOCKET_FD(-1) {}
+    TCP_Protocol(const string& IP, const int PORT) : IP_ADDRESS(IP), PORT(PORT), SOCKET_FD(-1) {}
 
-	// Getters & Setters
-	// 	Later...
+    // Destructor
+    virtual ~TCP_Protocol() {
+        closeSocket();
+    }
 
-	// -------------- Socket Creating Functions ----------------
-	// Returns true, if creates Socket, else false
-	bool createSocket() {
-		SOCKET_FD= socket(AF_INET, SOCK_STREAM, 0);
+    // Getters & Setters
+    // 	Later...
 
-		if(SOCKET_FD < 0) {
-			std::cerr << "Failed to create socket\n";
-			return false;
-		}
+    // -------------- Socket Creating Functions ----------------
+    // Returns true, if creates Socket, else false
+    bool createSocket() {
+        SOCKET_FD = socket(AF_INET, SOCK_STREAM, 0);
 
-		return true;
-	}
-	// Returns true, if closes an existing Socket, else false
-	bool closeSocket() {
-		if(SOCKET_FD < 0) {
-			return false;
-		}
+        if (SOCKET_FD < 0) {
+            std::cerr << "Failed to create socket\n";
+            return false;
+        }
 
-		close(SOCKET_FD);
-		SOCKET_FD= -1;
+        return true;
+    }
+    // Returns true, if closes an existing Socket, else false
+    bool closeSocket() {
+        if (SOCKET_FD < 0) {
+            return false;
+        }
 
-		return true;
-	}
+        close(SOCKET_FD);
+        SOCKET_FD = -1;
 
-	// --------------- Data Transfer Functions ---------------
-	// Returns true, if messag was sent successfully, else false
-	bool sendData(const string& message) {
-		ssize_t bytes_sent= send(
-			SOCKET_FD,
-			message.data(),
-			message.size(),
-			0		
-		);	
+        return true;
+    }
 
-		return (bytes_sent >= 0);
-	}
-	// Returns the message received, if buffer is received successfully, else an empty string
-	string receiveData() {
-		string buffer(1024, '\0');
+    // --------------- Data Transfer Functions ---------------
+    // Returns true, if messag was sent successfully, else false
+    bool sendData(const string& message) {
+        ssize_t bytes_sent = send(
+            SOCKET_FD,
+            message.data(),
+            message.size(),
+            0);
 
-		ssize_t bytes_received= recv(
-			SOCKET_FD,
-			&buffer[0],
-			buffer.size(),
-			0			
-		);
+        return (bytes_sent >= 0);
+    }
+    // Returns the message received, if buffer is received successfully, else an empty string
+    string receiveData() {
+        string buffer(1024, '\0');
 
-		if(bytes_received <= 0) {
-			return "";			
-		}
+        ssize_t bytes_received = recv(
+            SOCKET_FD,
+            &buffer[0],
+            buffer.size(),
+            0);
 
-		buffer.resize(bytes_received);
-		return buffer;
-	}
+        if (bytes_received <= 0) {
+            return "";
+        }
+
+        buffer.resize(bytes_received);
+        return buffer;
+    }
 };
--- src/client.cpp
+++ src/client.cpp
@@ -1,73 +1,69 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 #include "tcp_protocol.hpp"
 
 using namespace std;
 
-
-class Client: public TCP_Protocol {
+class Client : public TCP_Protocol {
 public:
-	Client(): TCP_Protocol() {}
-	Client(const string& IP_ADDRESS, const int PORT): TCP_Protocol(IP_ADDRESS, PORT) {}
+    Client() : TCP_Protocol() {}
+    Client(const string& IP_ADDRESS, const int PORT) : TCP_Protocol(IP_ADDRESS, PORT) {}
 
-	~Client() {
-		closeSocket(SOCKET_FD);
-	}
+    ~Client() {
+        closeSocket(SOCKET_FD);
+    }
 
-	bool connectToServer() {
-		sockaddr_in SERVER_ADDR{};
+    bool connectToServer() {
+        sockaddr_in SERVER_ADDR{};
 
-		SERVER_ADDR.sin_family= AF_INET;
-		SERVER_ADDR.sin_port= htons(PORT);
-		SERVER_ADDR.sin_addr.s_addr= inet_addr(IP_ADDRESS.c_str());
+        SERVER_ADDR.sin_family = AF_INET;
+        SERVER_ADDR.sin_port = htons(PORT);
+        SERVER_ADDR.sin_addr.s_addr = inet_addr(IP_ADDRESS.c_str());
 
-		if(
-			connect	(
-				SOCKET_FD,
-				reinterpret_cast<sockaddr*>(&SERVER_ADDR),
-				sizeof(SERVER_ADDR)
-			) < 0
-		) {
-			cerr << "Failed to connect to Server!\n";
-			return false;
-		}
+        if (
+            connect(
+                SOCKET_FD,
+                reinterpret_cast<sockaddr*>(&SERVER_ADDR),
+                sizeof(SERVER_ADDR)) < 0) {
+            cerr << "Failed to connect to Server!\n";
+            return false;
+        }
 
-		cout << "Connected to Server Succesfully!\n";
-		return true;
-	}
+        cout << "Connected to Server Succesfully!\n";
+        return true;
+    }
 
-	void run() {
-		if(!createSocket(SOCKET_FD)) {
-			return;
-		}
-		if(!connectToServer()) {
-			return;
-		}
+    void run() {
+        if (!createSocket(SOCKET_FD)) {
+            return;
+        }
+        if (!connectToServer()) {
+            return;
+        }
 
-		cout << "Enter Message: ";
-		string message;
-		getline(cin, message);
+        cout << "Enter Message: ";
+        string message;
+        getline(cin, message);
 
-		sendData(SOCKET_FD, message);
+        sendData(SOCKET_FD, message);
 
-		string response= receiveData(SOCKET_FD);
+        string response = receiveData(SOCKET_FD);
 
-		if(!response.empty()) {
-			cout << "Response: "
-			     << response
-			     << std::endl;
-		}
+        if (!response.empty()) {
+            cout << "Response: "
+                 << response
+                 << std::endl;
+        }
 
-		closeSocket(SOCKET_FD);
-	}
+        closeSocket(SOCKET_FD);
+    }
 };
 
-
 int main() {
-	Client client("127.0.0.1", 9090);
+    Client client("127.0.0.1", 9090);
 
-	client.run();
+    client.run();
 
-	return 0;
+    return 0;
 }
