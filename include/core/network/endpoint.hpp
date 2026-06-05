b314d8a85e5e6c2af575d8cee2965721f49109b6
VeylorisRuinarisNyctalis <veylorisruinarisnyctalis@gmail.com>
2026-06-05 11:55:05 +0530

    Imporved Endpoint class

--- include/core/network/endpoint.hpp
+++ include/core/network/endpoint.hpp
@@ -13,7 +13,15 @@ protected:
 
 public:
     Endpoint() : IP_ADDRESS("127.0.0.1"), PORT(8080) {}
-    Endpoint(const std::string& IP_ADDRESS, const uint16_t& PORT) : IP_ADDRESS(IP_ADDRESS), PORT(PORT) {}
+    Endpoint(const std::string& IP_ADDRESS, const uint16_t PORT) : IP_ADDRESS(IP_ADDRESS), PORT(PORT) {}
+    Endpoint(const sockaddr_in& addr) {
+        char ip[INET_ADDRSTRLEN];
+
+        inet_ntop(AF_INET, &addr.sin_addr, ip, sizeof(ip));
+
+        this->IP_ADDRESS = ip;
+        this->PORT = ntohs(addr.sin_port);
+    }
 
     virtual ~Endpoint() = default;
 
@@ -32,14 +40,26 @@ public:
         ADDR.sin_family = AF_INET;
         ADDR.sin_port = htons(PORT);
 
+        if (!isValidIP()) {
+            throw std::runtime_error("Invalid IP Address");
+        }
+
         inet_pton(
             AF_INET,
             this->IP_ADDRESS.c_str(),
             &ADDR.sin_addr);
-
         return ADDR;
     }
     socklen_t getSockAddrLen() const {
         return sizeof(sockaddr_in);
     }
-}:
+
+    bool isValidIP() const {
+        sockaddr_in addr;
+
+        return inet_pton(
+                   AF_INET,
+                   IP_ADDRESS.c_str(),
+                   &addr.sin_addr) == 1;
+    }
+};
