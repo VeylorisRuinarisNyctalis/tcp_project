983d9105ce8b26739f11a420490411844cda2e2f
VeylorisRuinarisNyctalis <veylorisruinarisnyctalis@gmail.com>
2026-06-05 11:54:42 +0530

    completed Connection class

---
+++ include/core/network/connection.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <string>
+
+#include "endpoint.hpp"
+#include "socket.hpp"
+
+class Connection {
+protected:
+    Endpoint ENDPOINT;
+    Socket SOCKET;
+
+public:
+    Connection() = default;
+    Connection(const string& ip, const int& port)
+        : ENDPOINT(IP, PORT) {}
+    Connection(int socket_fd, const sockaddr_in& addr)
+        : ENDPOINT(addr), SOCKET(socket_fd) {}
+
+    ~Connection() = default;
+
+    // Getters
+    const Endpoint& getEndpoint() const {
+        return ENDPOINT;
+    }
+    const Socket& getSocket() const {
+        return SOCKET;
+    }
+
+    // Data transfer
+    bool sendData(const std::string& message) {
+        if (!SOCKET.isValid()) {
+            return false;
+        }
+
+        ssize_t sent = send(
+            SOCKET.getFD(),
+            message.data(),
+            message.size(),
+            0);
+
+        return sent != -1;
+    }
+    std::string receiveData() {
+        if (!SOCKET.isValid()) {
+            return "";
+        }
+
+        char buffer[1024];
+
+        ssize_t bytes = recv(
+            SOCKET.getFD(),
+            buffer,
+            sizeof(buffer),
+            0);
+
+        if (bytes <= 0) {
+            return "";
+        }
+
+        return std::string(buffer, bytes);
+    }
+};
