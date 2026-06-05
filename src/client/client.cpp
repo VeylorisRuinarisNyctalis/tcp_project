e0c04a7392e35fd38a88ec012a176e25354c0124
VeylorisRuinarisNyctalis <veylorisruinarisnyctalis@gmail.com>
2026-06-05 14:42:46 +0530

    made runnables

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,71 +1,21 @@
-#include <iostream>
-#include <string>
-
-#include "tcp_protocol.hpp"
-
-using namespace std;
-
-class Client : public TCP_Protocol {
-public:
-    Client() : TCP_Protocol() {}
-    Client(const string& IP_ADDRESS, const int PORT) : TCP_Protocol(IP_ADDRESS, PORT) {}
-
-    ~Client() {
-        closeSocket(SOCKET_FD);
-    }
-
-    bool connectToServer() {
-        sockaddr_in SERVER_ADDR{};
-
-        SERVER_ADDR.sin_family = AF_INET;
-        SERVER_ADDR.sin_port = htons(PORT);
-        SERVER_ADDR.sin_addr.s_addr = inet_addr(IP_ADDRESS.c_str());
-
-        if (
-            connect(
-                SOCKET_FD,
-                reinterpret_cast<sockaddr*>(&SERVER_ADDR),
-                sizeof(SERVER_ADDR)) < 0) {
-            cerr << "Failed to connect to Server!\n";
-            return false;
-        }
-
-        cout << "Connected to Server Succesfully!\n";
-        return true;
-    }
-
-    void run() {
-        if (!createSocket(SOCKET_FD)) {
-            return;
-        }
-        if (!connectToServer()) {
-            return;
-        }
-
-        while (true) {
-            cout << "Enter Message: ";
-            string message;
-            getline(cin, message);
-
-            sendData(SOCKET_FD, message);
-
-            string response = receiveData(SOCKET_FD);
-
-            if (!response.empty()) {
-                cout << "Response: "
-                     << response
-                     << std::endl;
-            }
-        }
-
-        closeSocket(SOCKET_FD);
-    }
-};
+#include "client.hpp"
 
 int main() {
-    Client client("127.0.0.1", 9090);
+    Client CLIENT("127.0.0.1", 9090);
 
-    client.run();
+    CLIENT.createClientSocket();
+    CLIENT.connectToServer();
+
+    // Messaging
+    std::string request;
+    std::cout << "Enter Request: ";
+    getline(std::cin, request);
+
+    CLIENT.getConnection().sendData(request);
+
+    std::string response = CLIENT.getConnection().receiveData();
+
+    cout << "Request: " << request << std::endl;
 
     return 0;
 }
